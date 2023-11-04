ba2f446546159a318766ff1832dd0179f3e16d35
ryaasishlah <ryaasishlah123@gmail.com>
2023-11-04 19:43:32 +0700

    add template

---
+++ assets/js/template/template.js
@@ -0,0 +1,3 @@
+export let URLPost = `https://asia-southeast2-pasabar.cloudfunctions.net/registerpasabar`
+export let URLLogin = `https://asia-southeast2-pasabar.cloudfunctions.net/loginpasabar`
+export let token = 'token';
